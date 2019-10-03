560aa300a3f1bcb5d7d36bbe69899de147f5757a
James Ward <james@jamesward.com>
2019-10-03 15:58:34 -0600

    info on param not documented

--- cmd/redirector/referer.go
+++ cmd/redirector/referer.go
@@ -33,8 +33,11 @@ func prepURL(r repoRef, overrides url.Values) string {
 		Path:   "cloudshell/editor",
 	}
 	q := make(url.Values)
-	q.Set("cloudshell_image", "gcr.io/cloudrun/button")
+	// not an officially documented param:
+	// https://cloud.google.com/shell/docs/open-in-cloud-shell
 	q.Set("shellonly", "true")
+
+	q.Set("cloudshell_image", "gcr.io/cloudrun/button")
 	q.Set("cloudshell_git_repo", r.GitURL())
 	if v := r.Ref(); v != "" {
 		q.Set("cloudshell_git_branch", v)
